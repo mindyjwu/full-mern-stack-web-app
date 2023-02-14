cd62f972c633db8badcaf576080ba1870c7dfc96
Mindy Wu <mindy@Mindys-MBP.lan>
2023-02-14 16:39:55 -0500

    adding about us page

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 // import './Home.css'
 
 /**
@@ -10,10 +10,22 @@ const AboutUs = props => {
   return (
     <>
       <h1>About us</h1>
-      
+      <p>My name is Mindy, currently a senior at NYU doing Computer Science and Data Science with minor in Economics and Business Entertainment Media and Technology.</p>
+      <p>I love going to the gym, cook, drink, paint, and hanging out with friends. During weekend, I love hanging out at the park or museums because</p>
+      <p>the park has cute dogs, trees, and cute babies. I also love museum because of art </p>
+      <p>Occassionally clubs too because there is music, drinks, and my friends. </p>
+
+    <p>
+        Currently on campus, I am engaged in two main clubs: Taiwanese American Student Society and Data Science Club. Feel free to ask
+        me any questions regarding these two clubs or the events we are hosting this semester. 
+        I am leaving New York City end of the summer to move to LA for my job, but I am not certain on what I want to do in my life yet. 
+        I am super extroverted and love meeting new people, please do say hi to me. 
+        </p>
     </>
   )
 }
 
-export default AboutUs;
+// make this component available to be imported into any other file
+export default AboutUs
+
 // make this Aboutus
