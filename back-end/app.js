e452a2c2ff3cdeed8b4d5b2291326c44cddbdebd
Mindy Wu <mindy@Mindys-MBP.lan>
2023-02-17 00:31:24 -0500

    make changes to About Us

--- back-end/app.js
+++ back-end/app.js
@@ -77,6 +77,22 @@ app.post('/messages/save', async (req, res) => {
     })
   }
 })
+app.get('/AboutUs', async (req, res) => {
+  // load all messages from database
+  const paragraph1 = `My name is Mindy, currently a senior at NYU doing Computer Science and Data Science with minor in Economics and Business Entertainment Media and Technology.
+  I love going to the gym, cook, drink, paint, and hanging out with friends. During weekend, I love hanging out at the park or museums because
+  the park has cute dogs, trees, and cute babies. I also love museum because of art 
+  Occassionally clubs too because there is music, drinks, and my friends.`
+ 
+  const paragraph2 = `Currently on campus, I am engaged in two main clubs: Taiwanese American Student Society and Data Science Club. Feel free to ask
+  me any questions regarding these two clubs or the events we are hosting this semester. 
+  I am leaving New York City end of the summer to move to LA for my job, but I am not certain on what I want to do in my life yet.     
+  I am super extroverted and love meeting new people, please do say hi to me. `
+  res.json({
+    text1: paragraph1,
+    text2: paragraph2
+  }) 
+})
 
 // export the express app we created to make it available to other modules
 module.exports = app // CommonJS export style!
--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,28 +1,23 @@
 // import { Link } from 'react-router-dom'
 // import './Home.css'
+import { useState, useEffect } from 'react'
 
 /**
- * A React component that represents the Home page of the app.
+ * A React component that represents the AboutUd page of the app.
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
 const AboutUs = props => {
-  return (
-    <>
-      <h1>About us</h1>
-      <p>My name is Mindy, currently a senior at NYU doing Computer Science and Data Science with minor in Economics and Business Entertainment Media and Technology.</p>
-      <p>I love going to the gym, cook, drink, paint, and hanging out with friends. During weekend, I love hanging out at the park or museums because</p>
-      <p>the park has cute dogs, trees, and cute babies. I also love museum because of art </p>
-      <p>Occassionally clubs too because there is music, drinks, and my friends. </p>
-
-    <p>
-        Currently on campus, I am engaged in two main clubs: Taiwanese American Student Society and Data Science Club. Feel free to ask
-        me any questions regarding these two clubs or the events we are hosting this semester. 
-        I am leaving New York City end of the summer to move to LA for my job, but I am not certain on what I want to do in my life yet. 
-        I am super extroverted and love meeting new people, please do say hi to me. 
-        </p>
-    </>
-  )
+    const [aboutus, setAboutUs] = useState([])
+    const fetchData = () => {
+        axios
+          .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/AboutUs`)
+          .then(response => {
+            setAboutUs([response.data.paragraph1, response.data.paragraph2])
+          })}
+    return(<><h1>About Us</h1>
+    <p>{aboutus[0]}</p>
+    <p>{aboutus[1]}</p>  </>) //fragment incldue all the elements of an object
 }
 
 <img src="https://images.app.goo.gl/QUdXT4mE418RL7CW6" alt="React Image" />
